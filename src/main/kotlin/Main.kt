5a1b38e88703ac2249dbc1175017462faf572f8c
Alexander Sidorkin <sashel007@yandex.ru>
2023-05-14 14:02:59 +0300

    after corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,16 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val like: Int
+    val manCase = "человеку"
+    val peopleCase = "людям"
 
-    val scanner = Scanner(System.`in`)
+
     println("Введите количество лайков: ")
-    val likes = scanner.nextInt()
-
-    val peopleWord = if (likes == 1) "человеку" else "людям"
+    var likes = readln().toInt()
 
     var likesWord = when {
-        likes % 10 == 1 && likes % 100 != 11 -> print("Понравилось $likes $peopleWord")
-        likes % 10 in 2..4 && likes % 100 !in 12..14 -> print("Понравилось $likes $peopleWord")
-        else -> println("Понравилось $likes людям")
+        likes % 10 == 1 && likes % 100 != 11 -> print("Понравилось $likes $manCase.")
+        likes % 10 in 2..4 && likes % 100 !in 12..14 -> print("Понравилось $likes $peopleCase")
+        else -> println("Понравилось $likes $peopleCase")
     }
 }
